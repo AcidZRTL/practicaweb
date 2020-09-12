8692d413c58dc37343b396faa83575bc389e42ca
AcidZRTL <acidrztr@gmail.com>
2020-09-11 20:52:30 -0500

    Hola a todos

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Grupo Septimo';
+  frutas = ['Manzana','Fresa','Naranja','Uva'];
+}
